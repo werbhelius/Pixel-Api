f7c1f619f04b978a59945a07c686378d83ed8a87
wanbo <wanbowb@live.com>
2017-06-03 22:55:49 +0800

    remove mutiparty

--- router/send_image.js
+++ router/send_image.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express();
 const ApiRequest = require('../service/api_service')
 const ApiConfig = require('../api/api_config')
-const multiparty = require('multiparty');
 const multer = require('multer');
 const upload = multer({ dest: './public/images/user' });
 
